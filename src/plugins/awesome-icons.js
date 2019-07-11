2789e17cb12af056573e7e6d26e1e95a37c7b035
andrey31 <andreybm31@gmail.com>
2019-07-11 08:54:38 -0600

    Actualizado dependencias y agregado iconos => editar, eliminar

--- src/plugins/awesome-icons.js
+++ src/plugins/awesome-icons.js
@@ -6,6 +6,7 @@ import 'vue-awesome/icons/expand-arrows-alt'
 import 'vue-awesome/icons/trash-alt'
 import 'vue-awesome/icons/tasks'
 import 'vue-awesome/icons/cloud-upload-alt'
+import 'vue-awesome/icons/edit'
 
 import Icon from 'vue-awesome/components/Icon'
 
